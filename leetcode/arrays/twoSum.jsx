98ff47a45dd66ab145078bc61597d3fc96710ff8
WanjikuN <cikunjoroge4@gmail.com>
2026-07-09 08:44:34 +0300

    Update two sum solution

--- leetcode/arrays/twoSum.jsx
+++ leetcode/arrays/twoSum.jsx
@@ -1,31 +1,35 @@
 // Problem
 // Two Sum
-// Problem: Given an array of integers nums and an integer target, return the indices of the two numbers such that they add up to target. 
+// Problem: Given an array of integers nums and an integer target, return the indices of the two numbers such that they add up to target.
 
 // Input: nums = [2, 7, 11, 15], target = 9
 // Output: [0, 1] (because nums[0] + nums[1] == 9)
 // Constraints: You may not use the same element twice. You can return the answer in any order. Assume exactly one solution exists
 
 // Time Complexity
-// Linear O(n)- as time will increase proportionally to the input size
+// Quadratic O(n2)- nested loop
 // Space Complexity
 // constant O(1) - list does not grow as input grows
 // Lessons Learned
-// Find a solution first then optimize
-
+// Find a solution first(brute force) then optimize
+// 2+7
+// 7+11
+// 7+15
+// 11+15
+// i  j
+// 0  1
+// 1  2
+// 2  3
+// 3
 // Approach
-const twoSum = (s, target) =>{
-    let indicesArray = []
-    
-    // loop trough array
-    for(let i=0; i<s.length;i++){
-        if(s[i] == s.length -1) return
-        let x = s[i] + s[i+1]
-        if( x == target){
-            indicesArray.push(i)
-            indicesArray.push(i+1)
+const twoSum = (nums, target) => {
+    // Compare each number with every subsequent number.
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
         }
     }
-    return indicesArray
-}
-console.log(twoSum([2, 7, 11, 15],9))
+};
+console.log(twoSum([2, 7, 11, 15], 9));
