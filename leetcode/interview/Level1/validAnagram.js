c18448d49b74a69589fc74e8376f3f3933da6e9c
WanjikuN <cikunjoroge4@gmail.com>
2026-08-13 10:16:11 +0300

    Update leetcode anagram solution

---
+++ leetcode/interview/Level1/validAnagram.js
@@ -0,0 +1,36 @@
+// Valid Anagram
+// s = "anagram"
+// t = "nagaram"
+// → true
+
+// s = "rat"
+// t = "car"
+// → false
+
+// -> size
+// -> build a map for s and traverse t checking if it is there
+const validAnagram = (s, t) => {
+    const sMap = new Map();
+    if (s.length !== t.length) {
+        return false;
+    }
+    // frequency map
+    for (let i = 0; i < s.length; i++) {
+        sMap.set(s[i], sMap.get(s[i] || 0) + 1);
+    }
+    // traverse t
+    for (let i = 0; i < t.length; i++) {
+        if (sMap.has(t[i])) {
+            sMap.set(t[i], sMap.get(t[i]) - 1);
+        } else {
+            return false;
+        }
+    }
+    for (const val in sMap.values()) {
+        if (val !== 0) {
+            return false;
+        }
+    }
+    return true;
+};
+console.log(validAnagram("anagram","nagaram"))
